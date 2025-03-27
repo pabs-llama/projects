a1d523ad9023931a95568b146c544620820ea520
pabs-llama <pablosoriano1989@gmail.com>
2025-03-27 12:49:26 +0100

    sql

--- stock_api/streamlit_app.py
+++ stock_api/streamlit_app.py
@@ -3,20 +3,4 @@ from dotenv import load_dotenv
 import os
 import skl
 
-st.title(":blue[Crypto] Historical Data")
-# x = st.text_input ("Select your Crypto Currency")
-# st.write(f"you selected {x}")
-crypto_currency = st.selectbox(
-    "Select your Cryptocurrency",
-    ("Bitcoin (BTC)", "Ethereum (ETH)", "Tether (USDT)", "XRP (XRP)", "Binance Coin (BNB)"),
-    index=None,
-    placeholder="Select your Cryptocurrency"
-)
-
-st.header (f"{crypto_currency} data")
-st.text ("blabla")
-st.text ("more text")
-
-
-
-
+st
