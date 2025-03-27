ff373eade701640b5c2178f76f2f72f4af21a2fa
pabs-llama <pablosoriano1989@gmail.com>
2025-03-27 17:44:27 +0100

    streamlit app

--- stock_api/streamlit_app.py
+++ stock_api/streamlit_app.py
@@ -1,21 +1,54 @@
 import streamlit as st
+import pandas as pd
 from dotenv import load_dotenv
 import os
-import skl
+from sqlalchemy import create_engine, text
+
+load_dotenv()
+
+def get_api_data():
+    dbconn = os.getenv("DBCONN")
+    engine = create_engine(dbconn)
+  
+    query = text("SELECT date, open, close, symbol FROM api_crypto_data")
+
+    with engine.connect() as conn:
+        df = pd.read_sql (query,conn)
+    return df
+
+api_data_df = get_api_data()
 
 st.title(":blue[Crypto] Historical Data")
-# x = st.text_input ("Select your Crypto Currency")
-# st.write(f"you selected {x}")
-crypto_currency = st.selectbox(
-    "Select your Cryptocurrency",
-    ("Bitcoin (BTC)", "Ethereum (ETH)", "Tether (USDT)", "XRP (XRP)", "Binance Coin (BNB)"),
-    index=None,
-    placeholder="Select your Cryptocurrency"
+
+# crypto_currency = st.selectbox(
+#     "Select your Cryptocurrency",
+#     ("Bitcoin (BTC)", "Ethereum (ETH)", "Tether (USDT)", "XRP (XRP)", "SOLANA (SOL)"),
+#     index=None,
+#     placeholder="Select your Cryptocurrency"
+# )
+
+# pivoting table so each column will become a line, with date as index
+pivot_df = api_data_df.pivot_table (
+    index="date",
+    columns="symbol", 
+    values="close",
+    aggfunc="last"
 )
 
-st.header (f"{crypto_currency} data")
-st.text ("blabla")
-st.text ("more text")
+selected_cryptos = st.multiselect(
+    "Select Cryptocurrency:",
+    ["BTC", "ETH","USDT", "XRP","SOL"],#pivot_df.columns.tolist(),
+    placeholder="Select your Cryptocurrency",
+    # default="BTC"
+)
+
+st.header (f"{selected_cryptos[0]} data")
+st.subheader(":blue[{selected_cryptos}] Historical Data")
+
+if selected_cryptos:
+    st.line_chart(pivot_df[selected_cryptos])
+
+
 
 
 
