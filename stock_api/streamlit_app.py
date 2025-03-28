f1b972a1760bd3dd153d80c5f3169802faef2502
pabs-llama <pablosoriano1989@gmail.com>
2025-03-28 14:45:24 +0100

    wrapping in functions

--- stock_api/streamlit_app.py
+++ stock_api/streamlit_app.py
@@ -1,11 +1,14 @@
+# Importing Libraries
 import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine, text
 
+#Load dotenv
 load_dotenv()
 
+# Fetching API data
 def get_api_data():
     dbconn = os.getenv("DBCONN")
     engine = create_engine(dbconn)
@@ -18,6 +21,21 @@ def get_api_data():
 
 api_data_df = get_api_data()
 
+# Fetching Scrapping data
+def get_scrapping_data():
+    dbconn = os.getenv("DBCONN")
+    engine = create_engine(dbconn)
+  
+    query = text("SELECT title, link, snippet, date FROM ft_articles")
+
+    with engine.connect() as conn:
+        df = pd.read_sql (query,conn)
+    return df
+
+scrapping_data_df = get_scrapping_data()
+
+# Adding Title
+
 st.title(":blue[Crypto] Historical Data")
 
 # crypto_currency = st.selectbox(
@@ -27,7 +45,8 @@ st.title(":blue[Crypto] Historical Data")
 #     placeholder="Select your Cryptocurrency"
 # )
 
-# pivoting table so each column will become a line, with date as index
+# Creating a Line chart for each crypto
+##  pivoting table so each column will become a line, with date as index
 pivot_df = api_data_df.pivot_table (
     index="date",
     columns="symbol", 
@@ -35,21 +54,37 @@ pivot_df = api_data_df.pivot_table (
     aggfunc="last"
 )
 
+## Creating a multiselect box so user can choose between crytps
 selected_cryptos = st.multiselect(
     "Select Cryptocurrency:",
     ["BTC", "ETH","USDT", "XRP","SOL"],#pivot_df.columns.tolist(),
     placeholder="Select your Cryptocurrency",
-    # default="BTC"
+    default="BTC"
 )
-
+## Adding a Header to the chart
 st.header (f"{selected_cryptos[0]} data")
 st.subheader(":blue[{selected_cryptos}] Historical Data")
 
+## Plotting the linechart
 if selected_cryptos:
     st.line_chart(pivot_df[selected_cryptos])
 
+# Displaying a dataframe with the scraped data
 
-
-
-
+st.data_editor(
+    scrapping_data_df,
+    column_config={
+        "title":"Title",
+        "snippet":"Snippet",
+        "date":"Date",
+        "link": st.column_config.LinkColumn(
+            "Link", 
+            help= "Check the article",
+            validate = r"^https://[a-z]+\.streamlit\.app$",
+            max_chars=100,
+            display_text= "link"
+        ),
+    },
+    hide_index=True,
+    )
 
